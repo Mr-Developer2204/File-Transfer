d8154e617cfba2ce3b4ddad2821bbbea8181ad61
Mr-Developer2204 <59203865+Mr-Developer2204@users.noreply.github.com>
2020-09-14 10:43:24 +0530

    Added Splash Screen

--- app/src/main/java/com/example/filetransfer/MainActivity.java
+++ app/src/main/java/com/example/filetransfer/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.filetransfer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int Splash_screen_timeout= 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Code for Splash Screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(home);
+                finish();
+            }
+        },Splash_screen_timeout);
+
     }
 }
